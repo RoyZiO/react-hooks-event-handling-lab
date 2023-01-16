7cc31709f97dd04583aa155a88d5a60d2fce52b7
RoyZiO <nziokinroy@gmail.com>
2023-01-16 14:28:35 +0300

    Completed assignments

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,17 @@
 // Code Keypad Component Here
-
+import React from 'react'
 function Keypad (){
-    return (
-        <div></div>
+
+    function printPassword(e){
+        e.preventDefault()
+ 
+        console.log("Entering password...");
+     }
+     return (
+         <div>
+             <input type="password" onChange={printPassword}></input>
+         </div>
+   
     )
 }
 
